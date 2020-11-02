00b976005a2078609c658b8da11fd1acbf5a03b3
nanaama527 <72888369+nanaama527@users.noreply.github.com>
2020-11-02 10:06:30 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,13 +1,36 @@
 def prime?(num)
-    if num == 2
-        true
-    elsif num > 1 && !(num.even?)
-        if num / 1 == num && num/num == 1
-            true
-    else
-        false
+    i = 2
+    range = []
+    if num < 2
+       return false
     end
-else
-    false
-end
+    while i < num - 1
+        range << num % i
+        i = i + 1
+    end
+    if range.any? do |value|
+        value == 0
+    end
+    return false
+    else
+    return true
+    end
+
+
+
+
+
+
+
+    #     if num == 2
+#         true
+#     elsif num > 1 && !(num.even?)
+#         if num / 1 == num && num/num == 1
+#             false
+#     else
+#         true
+#     end
+#     else
+#         false
+#     end
 end

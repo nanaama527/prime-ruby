235ce25ea57dd2fab5fbc0126230487ded4df159
nanaama527 <72888369+nanaama527@users.noreply.github.com>
2020-11-02 08:23:00 -0500

    Update prime.rb

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+    if num == 2
+        true
+    elsif num > 1 && !(num.even?)
+        if num / 1 == num && num/num == 1
+            true
+    else
+        false
+    end
+else
+    false
+end
+end
